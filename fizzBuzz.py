7fe4ec9b1c11a3f2fb4c5fdb72b60fc5ee1d0d43
Surajit Roy <surajitff77@gmail.com>
2026-06-07 14:56:29 +0530

    Update fizzBuzz.py
    completed code by Surajit

--- fizzBuzz.py
+++ fizzBuzz.py
@@ -1,2 +1,21 @@
+def fizzORbuzz(num, d1, d2):
+  if num%d1 == 0 and num%d2 == 0:
+    print("FizzBuzz")
+  elif num%d1 == 0:
+    print("Fizz")
+  elif num%d2 == 0:
+    print("Buzz")
+  else:
+    print("Faaaaaaaaaah!")
 
-def fizzORbuzz():
+try:
+  num = int(input("Enter a number: "))
+  a = int(input("Enter first divisor: "))
+  b = int(input("Enter second divisor: "))
+
+  fizzORbuzz(num,a,b)
+
+except ValueError:
+  print("Enter valid integer number.")
+
+
