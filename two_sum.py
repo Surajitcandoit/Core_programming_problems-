9b16c2978cc72137b049a19172fb0235c1e2492a
Surajit Roy <surajitff77@gmail.com>
2026-06-07 15:29:29 +0530

    Update two_sum.py
    completed by Surajit 7.6.2026

--- two_sum.py
+++ two_sum.py
@@ -1,2 +1,21 @@
+def find_two(nums, target):
+  seen = {}
+  for i in nums:
+    rem = target - i
+    if i in seen:
+      return [seen[i], i]
 
-def find_two():
+    seen[rem] = i
+
+print("Enter all element separate with space→")
+nums = list(map(int, input().split()))
+
+x = int(input("Enter your Sum value: "))
+print("Two number are:")
+
+result = find_two(nums, x)
+if result != None:
+  print(result)
+else:
+  print("Sum value is not possible with any two number.\nTry again!")
+
