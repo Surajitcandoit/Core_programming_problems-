682b3c5ef6f11cdce16b85e0c0bbdcec8081c931
Surajit Roy <surajitff77@gmail.com>
2026-06-07 14:29:54 +0530

    Update anagram.py
    write and complete the code

--- anagram.py
+++ anagram.py
@@ -1,2 +1,18 @@
 
-def check_angram():
+def check_anagram(a, b):
+  a = sorted(a.strip())                                      first = "".join(a)
+  s = sorted(b.strip())
+  sec = "".join(s)
+  if first == sec:                                             print("Two word have the same anagram.")
+  else:
+    print("Two word don't have the same anagram.")
+
+
+a = input("Enter a word: ")
+b = input("Enter another word: ")
+
+if a.isalpha() and b.isalpha():
+  check_anagram(a, b)
+
+else:
+  print("Please enter two real-world words containing only alphabets")
